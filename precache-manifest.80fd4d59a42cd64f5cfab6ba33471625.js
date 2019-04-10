caa035d6dc6040fc7a69c2207fd1e1c747267cf7
Nicolas Bocquet <nicolas.fow@gmail.com>
2019-04-10 16:11:05 +0200

    Updates

---
+++ precache-manifest.80fd4d59a42cd64f5cfab6ba33471625.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a193c8c91bd20efdaf72",
+    "url": "/bingerwatcher/static/js/runtime~main.a193c8c9.js"
+  },
+  {
+    "revision": "7c1256eed8edda5c46aa",
+    "url": "/bingerwatcher/static/js/main.7c1256ee.chunk.js"
+  },
+  {
+    "revision": "717f02d94618e8cc6d68",
+    "url": "/bingerwatcher/static/js/1.717f02d9.chunk.js"
+  },
+  {
+    "revision": "7c1256eed8edda5c46aa",
+    "url": "/bingerwatcher/static/css/main.044045ad.chunk.css"
+  },
+  {
+    "revision": "cf8072c7582ebe678655dd803ad9eea0",
+    "url": "/bingerwatcher/index.html"
+  }
+];
